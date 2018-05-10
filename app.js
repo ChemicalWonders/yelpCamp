9b6b0e4f8bd415cb293d4d38a131d89d8962da41
Kevin Chan <kevinchan@Kevins-MBP.T-mobile.com>
2018-05-09 23:55:32 -0700

    added data persistance in yelpcamp

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express    = require("express"),
+	app        = express(),
+	bodyParser = require("body-parser"),
+	mongoose   = require("mongoose");
 
-var campgrounds = [
-		{name: "Salmon Creek", image: "http://www.californiasbestcamping.com/photos5/salmon_creek_camp.jpg"},
-		{name: "Grant Village", image: "https://media-cdn.tripadvisor.com/media/photo-s/0d/39/fc/d7/grant-village-campground.jpg"},
-		{name: "Dry River Campsite", image: "https://www.reserveamerica.com/webphotos/NH/pid270015/0/180x120.jpg"}
-	];
-
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+// 	{
+// 		name: "Salmon Creek",
+// 		image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"
+// 	}, function(err, campground) {
+// 		if(err) {
+// 			console.log(err);
+// 		}
+// 		else {
+// 			console.log("Newly created campground: ");
+// 			console.log(campground);
+// 		}
+// 	});
 
 app.get("/", function(req, res){
 	res.render("index");
 });
 
 app.get("/campgrounds", function(req, res){
-	res.render("campgrounds", {campgrounds: campgrounds});
+	// Get all campgrounds from DB
+	Campground.find({}, function(err, allCampgrounds){
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("campgrounds", {campgrounds: allCampgrounds});
+		}
+	});
+	
 });
 
 app.get("/campgrounds/new", function(req, res){
@@ -29,10 +55,17 @@ app.post("/campgrounds",function(req, res){
 	var image = req.body.imageURL;
 
 	var newCampground = { name: name, image: image}
-	campgrounds.push(newCampground);
-	console.log("New Campground Successfully Added");
-	//redirect back to campgrounds page
-	res.redirect("/campgrounds");
+	//Create a new campground and save to DB
+	Campground.create(newCampground, function(err, newlyCreatedCampground){
+		if(err){
+			console.log(err);
+		} else {
+			console.log("New Campground Successfully Added");
+			//redirect back to campgrounds page
+			res.redirect("/campgrounds");
+		}
+	});
+	
 });
 
 app.listen(8000, function(){
